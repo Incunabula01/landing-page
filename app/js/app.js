e5a01cdac5acfb647f843e176090883a546f24f4
Joe Widener <jwidener08@gmail.com>
2015-01-31 18:44:04 -0500

    Almost Done

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,51 @@
 'use strict';
 
 (function() {
+
+	var header = $('header');
+	var sticky = $('#sticky-header');
+
+	header.waypoint({
+		handler: function(direction){
+			if(direction === 'down'){
+				sticky.addClass('stuck');
+				$(this).css({
+					height: 60
+				}, 200);
+			} else {
+				sticky.removeClass('stuck');
+				$(this).css({
+					height: "auto"
+				});
+			}
+		},
+		offset: function(){
+			return -header.outerHeight();
+		}
+	}) 
+
+	$('#Two').waypoint({
+		handler: function(direction){
+			if(direction === 'down'){
+				$('.pull-quote').fadeIn({
+					easeInQuad: 200
+				});
+				return true;
+			}
+			return false;
+		}
+	});
+
+	$('#Three').waypoint({
+		handler: function(direction){
+			if(direction === 'down'){
+				$('p').fadeIn({
+					easeInQuad: 200
+				});
+				return true;
+			}
+			return false;
+		}
+	})
 	
 })(jQuery);
